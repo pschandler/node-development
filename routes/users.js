424d6e7e3ba3a726ce2dfe8d8c238da51d10718b
Patrick Schandler <pschandler@gmail.com>
2024-03-28 21:00:55 -0500

    updating title

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   console.log("Updating the console log to try and trigger automatic build.");
-  res.send("respond with a resource");
+  res.send("Get a new title already");
 });
 
 module.exports = router;
